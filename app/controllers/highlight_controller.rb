08f0360092f5d34dd244843a79c55830c8da79cb
skylarkJ <janaryndin@gmail.com>
2017-03-28 10:51:43 -0400

    Add resources favorites, Route - Highlights, Controller
    Added resources favorites and route to get highlights.
    Created index method in controller to query NHL team with 25 results.
    Accessed through api key. Rendered json with videos. In favorites
    controller corrected private syntax on favorite.

---
+++ app/controllers/highlight_controller.rb
@@ -0,0 +1,17 @@
+require 'yt'
+
+class HighlightController < ApplicationController
+  def index
+    if !params[:query]
+      render json: []
+    else
+      Yt.configure do |config|
+        config.api_key = 'AIzaSyDpo8wYdgmwZytFPijvzhDhuhFEWAVur5w'
+      end
+      videos = Yt::Collections::Videos.new
+
+      render json: videos.where(q: params[:query] + ' NHL Team').take(25)
+    end
+  end
+
+end
