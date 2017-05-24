20887bb62a20b7a299b70b2016792283f1d348f7
skylarkJ <janaryndin@gmail.com>
2017-05-24 19:33:58 -0400

    Change a limit on a search result

--- app/controllers/highlight_controller.rb
+++ app/controllers/highlight_controller.rb
@@ -10,7 +10,7 @@ class HighlightController < ApplicationController
       end
       videos = Yt::Collections::Videos.new
 
-      render json: videos.where(q: params[:query] + ' NHL Team').take(25)
+      render json: videos.where(q: params[:query] + ' NHL Team').take(24)
     end
   end
 
